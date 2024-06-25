71e69d77982bd9bf5a543bf3e35ee07db7c83e39
phamdat2004 <phamtiendat21072004@gmai.com>
2024-06-25 09:15:02 +0700

    upload

---
+++ Controllers/BanController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webbanhang.Controllers
+{
+    public class BanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult hienThi()
+        {
+            return View();
+        }
+        public IActionResult product1()
+        {
+            return View();
+        }
+        public IActionResult product2()
+        {
+            return View();
+        }
+        public IActionResult product3()
+        {
+            return View();
+        }
+    }
+}
+
